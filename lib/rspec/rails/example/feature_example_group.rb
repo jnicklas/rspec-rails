1d6934c0f23cec35e680ce7d76303c0a15b79e42
Jonas Nicklas <jonas.nicklas@gmail.com>
2012-11-09 16:51:56 +0100

    Add an error message when Capybara is not loaded

--- lib/rspec/rails/example/feature_example_group.rb
+++ lib/rspec/rails/example/feature_example_group.rb
@@ -8,5 +8,13 @@ module RSpec::Rails
 
       include Rails.application.routes_url_helpers
     end
+
+    def visit(*)
+      if defined?(super)
+        super
+      else
+        raise "Capybara not loaded, please add it to your Gemfile:\n\ngem \"capybara\""
+      end
+    end
   end
 end
--- spec/rspec/rails/example/feature_example_group_spec.rb
+++ spec/rspec/rails/example/feature_example_group_spec.rb
@@ -22,5 +22,29 @@ module RSpec::Rails
       end
       group.new.foo_path.should == "/foo"
     end
+
+    describe "#visit" do
+      it "raises an error informing about missing Capybara" do
+        group = RSpec::Core::ExampleGroup.describe do
+          include FeatureExampleGroup
+        end
+        expect do
+          group.new.visit('/foobar')
+        end.to raise_error(/Capybara not loaded/)
+      end
+
+      it "is resistant to load order errors" do
+        capybara = Module.new do
+          def visit(url)
+            "success: #{url}"
+          end
+        end
+        group = RSpec::Core::ExampleGroup.describe do
+          include capybara
+          include FeatureExampleGroup
+        end
+        group.new.visit("/foo").should == "success: /foo"
+      end
+    end
   end
 end
